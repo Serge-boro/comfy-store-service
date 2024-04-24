4b352db4222f73a94cc7372c50dd7012101bf827
Serge-boro <serhiiborodin83@gmail.com>
2024-04-23 21:00:43 -0400

    first commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express()
 //     // credentials: true,
 //   })
 // )
-app.use(express.static(path.resolve(__dirname, './public')))
+// app.use(express.static(path.resolve(__dirname, './public')))
 app.use(express.json())
 
 const corsConfig = {
@@ -41,9 +41,9 @@ app.use('/store', router)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './build', 'index.html'))
+// })
 
 const PORT = process.env.PORT || 5000
 
